6074f2e1e2e33965f5cb8a19a2272c74a7f4d5b3
K0lb3 <rkolbe96@gmail.com>
2023-06-07 11:34:43 +0200

    Animator - add MotionSkeletonMode

--- AssetStudio/Classes/Animator.cs
+++ AssetStudio/Classes/Animator.cs
@@ -22,6 +22,7 @@ namespace AssetStudio
                 var m_UpdateMode = reader.ReadInt32();
             }
 
+            var m_MotionSkeletonMode = reader.ReadInt32();
             var m_ApplyRootMotion = reader.ReadBoolean();
             if (version[0] == 4 && version[1] >= 5) //4.5 and up - 5.0 down
             {
